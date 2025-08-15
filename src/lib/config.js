ed8bc058c7706ca76a7fec6458d2fc75f458358d
CATTO NPM <nguyenphuquan229@gmail.com>
2025-08-16 06:52:38 +0700

    Add files via upload

--- src/lib/config.js
+++ src/lib/config.js
@@ -3,7 +3,7 @@ const status = "production";
 
 export default {
     rpcUrl: mode === "mainnet" && status === "production" ? "https://api.coregen.cc" : "http://localhost:443",
-    indexerUrl: mode === "mainnet" && status === "production" ? "http://localhost:444" : "http://localhost:444",
+    indexerUrl: mode === "mainnet" && status === "production" ? "https://indexer.coregen.cc" : "http://localhost:444",
     chainId: mode === "mainnet"  ? "0x45c" : "",
     ethRpcUrl: mode === "mainnet"  ? "https://rpc.coredao.org" : "",
     chainName: mode === "mainnet"  ? "Core Blockchain" : "Core Testnet",
@@ -13,10 +13,8 @@ export default {
         decimals: 18
     },
     blockExplorerUrls: "https://scan.coredao.org",
-    site: "https://corearena.xyz",
-    twitter: "https://x.com/corearena_xyz",
+    site: "https://coregen.cc",
+    twitter: "https://x.com/coregen_cc",
     discord: "https://discord.gg/eNXuhN22JX"
 }
 
-//http://localhost:443
-//https://api.corearena.xyz
